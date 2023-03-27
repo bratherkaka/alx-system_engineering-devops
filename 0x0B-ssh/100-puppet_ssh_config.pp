ff2a1e2a23931fecd0b417e08051295f343aed64
Josph Simon <kakabrather@gmail.com>
2023-03-27 17:54:10 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,14 @@
-file_line { 'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => '  IdentityFile ~/.ssh/school',
-  match => '^#?\s*IdentityFile',
+# Make passwordAuthentication and add new private key
+file_line { 'replace passwordAuthentication':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  replace => true,
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
 }
 
-file_line { 'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => '  PasswordAuthentication no',
-  match => '^#?\s*PasswordAuthentication',
+file_line { 'add the private key ~/.ssh/school':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+    line => 'IdentityFile ~/.ssh/school',
 }
